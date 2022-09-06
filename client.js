3bb0c38a8f7ab4973bd42e0ff084eb9d8fe62afd
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-06 13:40:24 -0500

    added a comment

--- client.js
+++ client.js
@@ -1,3 +1,20 @@
 /// <reference path="jQuery.js" />
 
 console.log('Here are all the available people:', people);
+
+$(readyNow);
+
+function readyNow () {
+}
+
+// <img src="https://github.com/booherbg.png?size=250" alt="Profile image of Blaine">
+
+function displayPictures () {
+
+    // append each person in the people object image to the DOM.
+    $('body').append(`
+        <img src=""/>`);
+}
+
+
+
