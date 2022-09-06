ef62d170fdc71b0859859a492da0fa4b3db6a690
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-06 13:49:27 -0500

    added start game button

--- client.js
+++ client.js
@@ -8,15 +8,19 @@ function readyNow () {
     displayPictures();
 }
 
-// <img src="https://github.com/booherbg.png?size=250" alt="Profile image of Blaine">
 
 function displayPictures () {
     // loop over each person in the people array.
     for (let person of people) {
         // append each person in the people object image to the DOM.
-        $('body').append(`
+        $('#pictures').append(`
         <img data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">`);
     }
+}
+
+function startGame(){
+    
+
 }
 
 
