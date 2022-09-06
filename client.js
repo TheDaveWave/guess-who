2d10cde6555145075d53542f731433588c60e77b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-06 14:37:15 -0500

    got check if img clicked is the correct person to work

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-// / <reference path="jQuery.js" />
+/// <reference path="jQuery.js" />
 
 console.log('Here are all the available people:', people);
 
@@ -7,6 +7,7 @@ $(readyNow);
 function readyNow () {
     displayPictures();
     $('#startButton').on('click', startGame);
+    $('#pictures').on('click', '.imgClass', checkImage);
 }
 
 
@@ -15,29 +16,41 @@ function displayPictures () {
     for (let person of people) {
         // append each person in the people object image to the DOM.
         $('#pictures').append(`
-        <img data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">`);
+        <img class="imgClass" data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">`);
     }
 }
+
 
 
 // make a feature that does not allow the same name to appear twice in a row.
 
 
 // prompt user with a random person.name and display it on the DOM.
+let mysteryPerson = '';
 
-function startGame(){
+function startGame(event){
     // set variable to a random number between 0 and the the length of the array - 1.
     let rdmNum = randomNumber(0, people.length-1);
     //TEST: console.log('randomNumber:', rdmNum);
 
     // assign random name using rdmNum variable to our mystery person
-    let mysteryPerson = people[rdmNum].name;
+    mysteryPerson = people[rdmNum].name;
     //console.log('Random Person', mysteryPerson);
 
     // append mysterPerson to DOM.
-    $('#gameText').text(`Pick the correct picture belonging to ${mysteryPerson}!`);
-    
+    $('#currentName').text(mysteryPerson);
+}
 
+
+
+function checkImage (event) {
+    let clickedImage = $(event.target).data('name');
+    console.log(clickedImage);
+    if(mysteryPerson === clickedImage) {
+        console.log('Correct!');
+    } else {
+        console.log('Wrong!');
+    }
 }
 
 // handy function that generates a random number given a min and max value.
