9487f7ec106268f32e5a68e8008d573be77be921
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-06 13:44:43 -0500

    successfully added profile images of each person onto the DOM

--- client.js
+++ client.js
@@ -5,15 +5,18 @@ console.log('Here are all the available people:', people);
 $(readyNow);
 
 function readyNow () {
+    displayPictures();
 }
 
 // <img src="https://github.com/booherbg.png?size=250" alt="Profile image of Blaine">
 
 function displayPictures () {
-
-    // append each person in the people object image to the DOM.
-    $('body').append(`
-        <img src=""/>`);
+    // loop over each person in the people array.
+    for (let person of people) {
+        // append each person in the people object image to the DOM.
+        $('body').append(`
+        <img data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">`);
+    }
 }
 
 
